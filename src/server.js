e3599f7542d888bd1608815477e13bc28f1d04d7
MatheusGxx <matheussoaresfff02@gmail.com>
2023-04-02 05:57:12 -0300

    Comendo o  Cors para permitir e restringir acesso a API

--- src/server.js
+++ src/server.js
@@ -23,13 +23,13 @@ const origincors = [
 // CONCEITO DE CORS = CORS É O O CONCEITO DE QUEM VAI PODER ACESSAR SUA API, SE SUA API VAI SER PUBLICA OU SE SUA API VAI SER UMA API PRIVADA QUE SOMENTE O SEU SERVIÇO PODERA ACESSAR ELA.
 app.use(cors({ // Mostrando quem pode acessar a API e quem nao pode
     origin: function (origin, callback ){
-        let allowed = true
+        let allowed = true // permitindo o acesso // allowed true = permitindo acesso allowed false = bloqueio do acesso 
 
-        if(!origin) allowed = true
+        if(!origin) allowed = true // se nao tiver nenhum servidor mandando request ele passa ( mobile )
 
-        if(!origincors.includes(origin)) allowed = false
+        if(!origincors.includes(origin)) allowed = false // se o link da request nao estiver dentro do array o allowed sera falso e ele nao tera acesso a API
 
-        callback(null, allowed)
+        callback(null, allowed) 
     }
 }))
 
